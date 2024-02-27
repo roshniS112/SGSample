a0d4a2f1edd89a42c01fecfabfb35f6cea914761
Roshni S <roshni.s1@ibm.com>
2024-02-27 15:57:17 +0530

    2 ssh key changes check

--- SeleniumProject/src/test/java/activities/Sample.java
+++ SeleniumProject/src/test/java/activities/Sample.java
@@ -6,6 +6,6 @@ public class Sample {
    // THis is sample file
     public static void main(String args[])
     {
-        System.out.print("New code from personal email id added");
+        System.out.print("New code is added - 2 sshekeys check");
     }
 }
